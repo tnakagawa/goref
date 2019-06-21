adbf41cd22c345030f36b218e12b8975d4bf8ed5
tnakagawa <nakagat@gmail.com>
2019-06-21 13:31:34 +0900

    add ecdsa

---
+++ ecdsa/ecdsa_test.go
@@ -0,0 +1,50 @@
+package ecdsa_test
+
+import (
+	"crypto/rand"
+	"testing"
+
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/tnakagawa/goref/ec"
+	"github.com/tnakagawa/goref/ecdsa"
+)
+
+func TestECDSA(t *testing.T) {
+	loop := 100
+	for i := 0; i < loop; i++ {
+		m := make([]byte, 32)
+		rand.Read(m)
+		hash := ecdsa.H(m)
+		key, err := btcec.NewPrivateKey(btcec.S256())
+		if err != nil {
+			t.Errorf("%v", err)
+			return
+		}
+		// btcec sign
+		sig, err := key.Sign(hash)
+		if err != nil {
+			t.Errorf("%v", err)
+			return
+		}
+		// sign
+		r, s := ecdsa.Sign(m, key.D)
+		// btcec verify
+		sig, err = btcec.ParseDERSignature(ecdsa.DER(r, s), btcec.S256())
+		if err != nil {
+			t.Errorf("%v", err)
+			return
+		}
+		result := sig.Verify(hash, key.PubKey())
+		if !result {
+			t.Errorf("verify error %v", result)
+			return
+		}
+		// verify
+		P := ec.Mul(key.D, ec.G)
+		result = ecdsa.Verify(P, m, sig.R, sig.S)
+		if !result {
+			t.Errorf("verify error %v", result)
+			return
+		}
+	}
+}
